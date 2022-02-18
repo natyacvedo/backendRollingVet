ba1cff45da553c91939045e880c1d3e562f77148
Naty Acevedo <natyacvedo@gmail.com>
2022-02-18 02:43:50 -0300

    se configuro el puerto y se instalaron dependencias

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("prueba")
+import express from "express";
+
+const app = express();
+app.set('port', process.env.PORT || 4000);
+
+app.listen( app.get('port'), () => {
+    console.log('puerto '+ app.get('port'));
+})
