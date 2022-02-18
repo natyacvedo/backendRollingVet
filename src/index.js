5831532b2cf6f7d78bd7bc504372571fbf466eea
Naty Acevedo <natyacvedo@gmail.com>
2022-02-18 03:17:17 -0300

    se instalo morgan y cors y se agregaron midlewares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 app.set('port', process.env.PORT || 4000);
 
 app.listen( app.get('port'), () => {
     console.log('puerto '+ app.get('port'));
+})
+
+//midlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/turnos',(req, res)=>{
+    res.send('lista de turnos');
 })
