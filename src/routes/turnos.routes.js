a11b5285db88708b6563ead9db5718209de98361
marcos medina <marcosjmedina@gmail.com>
2022-03-01 16:45:45 -0300

    ruta turnos funcionando, get post put delete

--- src/routes/turnos.routes.js
+++ src/routes/turnos.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import turnoCtrl from '../controllers/turnos.controllers';
+import turnoCtrl from "../controllers/turnos.controllers";
 
 const rutas = Router();
 
-rutas.route('/ListadoTurnos').get(turnoCtrl.listadoTurnos).post(turnoCtrl.crearTurno)
+rutas
+  .route("/ListadoTurnos")
+  .get(turnoCtrl.listarTurnos)
+  .post(turnoCtrl.crearTurno);
 
-
+rutas
+  .route("/ListadoTurnos/:id")
+  .get(turnoCtrl.obtenerTurno)
+  .put(turnoCtrl.editarTurno)
+  .delete(turnoCtrl.borrarTurno);
 
 export default rutas;
